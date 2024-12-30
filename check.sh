fbcf4cbd990a272f1ce5362204fb01ab0a7e2de6
Marat Shakirov <marat@shakirov.name>
2024-12-30 20:04:17 +0300

    Add check.sh to test services

---
+++ check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Проверка User Service
+echo "Testing User Service: Login"
+grpcurl -plaintext -d '{"username": "test", "password": "1234"}' localhost:50051 user.UserService/Login
+
+# Проверка Order Service
+echo "Testing Order Service: CreateOrder"
+grpcurl -plaintext -d '{"user_id": "123", "product_id": "p1", "quantity": 2}' localhost:50052 order.OrderService/CreateOrder
+
+echo "Testing Order Service: GetOrderStatus"
+grpcurl -plaintext -d '{"order_id": "order-123"}' localhost:50052 order.OrderService/GetOrderStatus
