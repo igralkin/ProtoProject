c3ccebdaa26a3dd2e230cee195f587f4799bba4e
Marat Shakirov <marat@shakirov.name>
2024-12-30 20:00:53 +0300

    Add Reflection API

--- order_service/order_service.py
+++ order_service/order_service.py
@@ -2,6 +2,8 @@ from concurrent import futures
 import grpc
 import order_service_pb2
 import order_service_pb2_grpc
+from grpc_reflection.v1alpha import reflection
+
 
 class OrderService(order_service_pb2_grpc.OrderServiceServicer):
     def CreateOrder(self, request, context):
@@ -19,6 +21,13 @@ class OrderService(order_service_pb2_grpc.OrderServiceServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     order_service_pb2_grpc.add_OrderServiceServicer_to_server(OrderService(), server)
+
+    SERVICE_NAMES = (
+        order_service_pb2.DESCRIPTOR.services_by_name['OrderService'].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
+
     server.add_insecure_port('[::]:50052')
     server.start()
     server.wait_for_termination()
--- user_service/user_service.py
+++ user_service/user_service.py
@@ -2,6 +2,8 @@ from concurrent import futures
 import grpc
 import user_service_pb2
 import user_service_pb2_grpc
+from grpc_reflection.v1alpha import reflection
+
 
 class UserService(user_service_pb2_grpc.UserServiceServicer):
     def Login(self, request, context):
@@ -18,12 +20,21 @@ class UserService(user_service_pb2_grpc.UserServiceServicer):
             email="test_user@example.com"
         )
 
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     user_service_pb2_grpc.add_UserServiceServicer_to_server(UserService(), server)
+
+    SERVICE_NAMES = (
+        user_service_pb2.DESCRIPTOR.services_by_name['UserService'].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
+
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+
 
 if __name__ == "__main__":
     serve()
